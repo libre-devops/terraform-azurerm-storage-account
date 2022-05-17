360f0ca638e68cfc7e76c81f248bf3dc3eef2983
Craig Thacker <craigthackerx@gmail.com>
2022-05-17 22:45:03 +0100

    Update

--- terraform/build.tf
+++ terraform/build.tf
@@ -7,3 +7,71 @@ module "rg" {
 
   #  lock_level = "CanNotDelete" // Do not set this value to skip lock
 }
+
+data "http" "user_ip" {
+  url = "https://ipv4.icanhazip.com" // If running locally, running this block will fetch your outbound public IP of your home/office/ISP/VPN and add it.  It will add the hosted agent etc if running from Microsoft/GitLab
+}
+
+module "network" {
+  source = "registry.terraform.io/libre-devops/network/azurerm"
+
+  rg_name  = module.rg.rg_name // rg-ldo-euw-dev-build
+  location = module.rg.rg_location
+  tags     = local.tags
+
+  vnet_name     = "vnet-${var.short}-${var.loc}-${terraform.workspace}-01" // vnet-ldo-euw-dev-01
+  vnet_location = module.network.vnet_location
+
+  address_space   = ["10.0.0.0/16"]
+  subnet_prefixes = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names    = ["sn1-${module.network.vnet_name}", "sn2-${module.network.vnet_name}", "sn3-${module.network.vnet_name}"] //sn1-vnet-ldo-euw-dev-01
+  subnet_service_endpoints = {
+    "sn1-${module.network.vnet_name}" = ["Microsoft.Storage"] // Adds extra subnet endpoints to sn1-vnet-ldo-euw-dev-01
+    "sn2-${module.network.vnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"], // Adds extra subnet endpoints to sn2-vnet-ldo-euw-dev-01
+    "sn3-${module.network.vnet_name}" = ["Microsoft.AzureActiveDirectory"] // Adds extra subnet endpoints to sn3-vnet-ldo-euw-dev-01
+  }
+}
+
+module "sa" {
+  source = "registry.terraform.io/libre-devops/storage-account/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  storage_account_name = "st${var.short}${var.loc}${terraform.workspace}01"
+  access_tier          = "Hot"
+  identity_type        = "SystemAssigned"
+
+  storage_account_properties = {
+
+    // Set this block to enable network rules
+    network_rules = {
+      default_action = "Deny"
+      bypass         = ["AzureServices", "Metrics", "Logging"]
+      ip_rules       = [chomp(data.http.user_ip.body)]
+      subnet_ids     = [element(values(module.network.subnets_ids), 0)]
+    }
+
+    blob_properties = {
+      versioning_enabled       = false
+      change_feed_enabled      = false
+      default_service_version  = "2020-06-12"
+      last_access_time_enabled = false
+
+      deletion_retention_policies = {
+        days = 10
+      }
+
+      container_delete_retention_policy = {
+        days = 10
+      }
+    }
+
+    routing = {
+      publish_internet_endpoints  = false
+      publish_microsoft_endpoints = true
+      choice                      = "MicrosoftRouting"
+    }
+  }
+}
